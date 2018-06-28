0747644e97f4e43307cb1a030db1456b0803c778
Timucin Acar <timucins_map@hotmail.de>
2018-06-28 19:30:21 +0200

    debuging response from volley

--- app/src/main/java/com/example/timuc/quicktyper/Services/GameService.kt
+++ app/src/main/java/com/example/timuc/quicktyper/Services/GameService.kt
@@ -1,10 +1,13 @@
 package com.example.timuc.quicktyper.Services
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.timuc.quicktyper.Utilities.BOARDCAST_GAME_START
 import com.example.timuc.quicktyper.Utilities.FIND_WORD_URL
 import org.json.JSONException
 import java.util.*
@@ -17,32 +20,36 @@ object GameService {
 
 
     fun findRandomWord(context: Context, complete: (Boolean) -> Unit){
-        val randomChar = getRandomChar()
-        var responseWord = ""
-        println(randomChar) //<---- TESTING
 
-        val randomWordRequest = object : JsonObjectRequest(Method.GET,  "$FIND_WORD_URL$randomChar", null, Response.Listener { response ->
+        var randomWord = getRandomChar() + "???"
+        println(randomWord)
+
+        val randomWordRequest = object : JsonObjectRequest(Method.GET, "$FIND_WORD_URL$randomWord", null, Response.Listener {response ->
             try {
-                println(response.toString()) //<---- TESTING
-                responseWord = response.getString("word")
+
+                var tmp = response.getString("word")
+                println(tmp)
+                word = tmp.replaceRange(1, tmp.length, "...")
+                println(word)
+
+                LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(BOARDCAST_GAME_START))
                 complete(true)
-            }catch (e: JSONException){
-                Log.d("JSON", "EXC:  ${e.localizedMessage}")
+
+            }catch (e : JSONException){
+                Log.d("JSON", "EXC: " + e.localizedMessage)
             }
+
         }, Response.ErrorListener { error ->
-            Log.d ("ERROR", "Could not get a Randomword: $error")
+            Log.d("ERROR", "Could not find user: $error")
             complete(false)
         }){
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
             }
+            }
+
+            Volley.newRequestQueue(context).add(randomWordRequest)
         }
-
-        Volley.newRequestQueue(context).add(randomWordRequest)
-        word = responseWord.replaceRange(1, responseWord.length, "...")
-        println(word) //<---- TESTING
-
-    }
 
     private fun getRandomChar(): Char {
         val alphabet ="abcdefghijklmnopqrstuvwxyz"
@@ -51,8 +58,4 @@ object GameService {
 
     }
 
-    private fun cutWord(){
-
     }
-
-}
